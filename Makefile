5e6e32db99e3adec30814e9cd64083c9fbae9259
Paul Knight <pknight@apple.com>
2016-05-27 22:38:21 +0800

    Add a Node frontend to run CRISP-8 tests and a new Makefile rule "test" that runs the tests. (There are no tests just yet.)

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 all: node
 
-node: crisp8node
+node: crisp8node crisp8testnode
 
 crisp8node:
 	tsc --out build/crisp8.js --sourceMap src/node/crisp8node.ts
+
+crisp8testnode:
+	tsc --out build/crisp8test.js --sourceMap src/node/crisp8testnode.ts
+
+test: crisp8testnode
+	node build/crisp8test.js tests/crisp8
 
 clean:
 	rm -rf build
---
+++ src/node/crisp8testnode.ts
@@ -0,0 +1,44 @@
+/// <reference path="NodeUtilities.ts" />
+/// <reference path="NodeOutputTestRunner.ts" />
+/// <reference path="../core/Crisp8OutputTest.ts" />
+
+function crisp8testmain() {
+    let options = new NodeOutputTestRunnerOptions();
+
+    let optionCallback = function(option: string, value: string) {
+        if (option === "--help") {
+            printUsageAndExit(0);
+        } else if (option === "--verbose") {
+            options.verbose = true;
+        } else {
+            console.error("Unrecognized option " + option);
+            printUsageAndExit(1);
+        }
+    };
+
+    let errorCallback = function(errorString: string) {
+        console.error(errorString);
+        printUsageAndExit(1);
+    };
+
+    let remainingArguments = NodeUtilities.parseArguments([], optionCallback, errorCallback);
+    let testRunner = new NodeOutputTestRunner(Crisp8OutputTest, remainingArguments, options);
+    testRunner.run();
+}
+
+function printUsageAndExit(status: number) {
+    console.log("CRISP-8: A CHIP-8 emulator.");
+    console.log("  Command line test runner.");
+    console.log("Usage: node crisp8test.js [--rebase] tests...");
+    console.log("");
+    console.log("Looks for files with a \".test\" extension and runs them in the CRISP-8 emulator.");
+    console.log("The test continues until the program jumps to 0x0111.");
+    console.log("The internal machine state is compared to the contents of the sibling file with a \".expected\" extension.");
+    console.log("");
+    console.log("Options: ");
+    console.log("  --rebase: Saves the actual internal machine state to the .expected file.");
+    console.log("  --verbose: Enable additional logging.");
+    process.exit(status);
+}
+
+crisp8testmain();

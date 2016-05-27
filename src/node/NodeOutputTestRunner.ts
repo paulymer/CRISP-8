58dddb28267a844aa2dc907ab7d57c47f6c81b79
Paul Knight <pknight@apple.com>
2016-05-27 22:35:01 +0800

    Add a Node platform implemenation of NodeOutputTestRunnerContext.
    Given a list of search paths and a concrete implementation of OutputTest, creates an OutputTest for each ".test" file in those search paths. Expected output should be a sibling file for each ".test" file with the ".expected" extension. Progress is reported via the console.

---
+++ src/node/NodeOutputTestRunner.ts
@@ -0,0 +1,93 @@
+/// <reference path="../core/PlatformOutputTestRunner.ts" />
+/// <reference path="PathParts.ts" />
+
+class NodeOutputTestRunnerOptions {
+    public verbose: boolean;
+    public rebase: boolean;
+
+    constructor() {
+        this.verbose = false;
+        this.rebase = false;
+    }
+}
+
+class NodeOutputTestRunnerContext {
+    public testPath: string;
+    public expectedPath: string;
+}
+
+class NodeOutputTestRunner extends PlatformOutputTestRunner {
+    private _testClass: OutputTestConstructor;
+    private _options: NodeOutputTestRunnerOptions;
+
+    private _testPaths: Array<string>;
+
+    constructor(testClass: OutputTestConstructor, searchPaths: Array<string>, options: NodeOutputTestRunnerOptions) {
+        super();
+        this._testClass = testClass;
+        this._options = options;
+
+        this._testPaths = new Array<string>();
+        for (let searchPath of searchPaths) {
+            for (let candidatePath of NodeUtilities.recursiveReadPathSync(searchPath)) {
+                if (path.extname(candidatePath) === ".test") {
+                    this._testPaths.push(candidatePath);
+                }
+            }
+        }
+    }
+
+    loadNextTest() {
+        let testPath = this._testPaths.shift();
+        if (testPath === undefined) {
+            return undefined;
+        }
+
+        let expectedPathParts = new PathParts(testPath);
+        expectedPathParts.extname = ".expected";
+        let expectedPath = expectedPathParts.toString();
+
+        let context = new NodeOutputTestRunnerContext;
+        context.testPath = testPath;
+        context.expectedPath = expectedPath;
+
+        let input = fs.readFileSync(testPath);
+        let expected = fs.readFileSync(expectedPath, { "encoding": "utf8" });
+
+        let test = new this._testClass(input, expected);
+        test.testRunnerContext = context;
+        return test;
+    }
+
+    testDidStart(test: OutputTest) {
+        let context = test.testRunnerContext as NodeOutputTestRunnerContext;
+        if (this._options.verbose) {
+            console.log("Starting test " + context.testPath);
+        }
+    }
+
+    testDidFail(test: OutputTest, result: OutputTestResult) {
+        let context = test.testRunnerContext as NodeOutputTestRunnerContext;
+        console.log("Test " + context.testPath + " failed.");
+        if (result === OutputTestResult.OutputTestResultFailedTimeout) {
+            console.log("Timed out.");
+        } else if (result === OutputTestResult.OutputTestResultFailedIncorrectResult) {
+            console.log("Expected result is:");
+            console.log(test.expectedOutput());
+            console.log("Actual result was:");
+            console.log(test.actualOutput());
+        }
+    }
+
+    testDidPass(test: OutputTest) {
+        let context = test.testRunnerContext as NodeOutputTestRunnerContext;
+        if (this._options.verbose) {
+            console.log("Test " + context.testPath + " passed.");
+        }
+    }
+
+    testingDidFinish() {
+        console.log("Testing finished. " + this.passedTestCount() + " passed / " + this.failedTestCount() + " failed.");
+    }
+}
+
